7294fcc49d1c3f92b088bb52f4eaa47bc50b1a70
genuine_code <113706866+genuinecode1@users.noreply.github.com>
2024-07-24 06:08:26 +0000

    Day1

---
+++ Day1.js
@@ -0,0 +1,56 @@
+console.log("hello")
+
+const accountId = 144553
+// in const once you give a value to variable it can't be modify
+
+let accountEmail = "xyz@gmail.com"
+
+var accountPassword = "12345"
+// we use let often more then var because var have scope issue(block scope and functional scope)
+
+accountCity = "Jaipur"
+// this is allowed by js but this is not a good practice
+
+
+// accountId = 2   not allowed
+// accountEmail = "abc@gmail.com"  allows
+// accountPassword = "2122222"     allows
+// accountCity = "Kanpur"          allows
+
+console.log(accountId);
+
+
+"use strict"  // treat all JS code as newer version
+
+// alert(3+3)  / we are using using nodejs, not browser
+
+// Datatypes ------>
+
+// string
+let name = "aditya"
+
+//number
+let age = 18
+
+//boolean
+let isLoggedIn = false
+
+//number -> 2 to power 53
+// bigint
+//  string = ""
+// boolean -> true/false
+// null -> stand-alone value
+// undefined
+//  symbol -> unique
+
+
+//object
+
+console.log(typeof "hitesh");
+
+console.log(typeof null)
+// type of null is object
+
+console.log(typeof undefined);
+// type of undefined is undefined
+
